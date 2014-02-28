7bc29467dcac61118c9f6dfcf0bb88a56ca55b15
lungetech <bmc@lungetech.com>
2014-02-28 09:40:32 -0500

    remove commented out code

--- virtual-machines/freeradius/prereqs/virtualbox.sh
+++ virtual-machines/freeradius/prereqs/virtualbox.sh
@@ -1,11 +1,5 @@
 #!/bin/bash -eux
 
-#/etc/init.d/virtualbox-ose-guest-utils stop
-#rmmod vboxguest
-#apt-get -y purge virtualbox-ose-guest-x11 virtualbox-ose-guest-dkms virtualbox-ose-guest-utils
-#apt-get -y install dkms
-
-echo "Building VBOX Additions"
 mount -o loop VBoxGuestAdditions_$(cat /home/vagrant/.vbox_version).iso /mnt
 yes | sh /mnt/VBoxLinuxAdditions.run
 umount /mnt
